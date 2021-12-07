6ca97777e42654d1d6e9ddc25c9988692f66e2b0
abrave1113 <abrave1113@gmail.com>
2021-12-07 18:09:10 -0500

    reorder

--- server/index.js
+++ server/index.js
@@ -18,15 +18,14 @@ app.use(bodyParser.json({limit:"30mb", extended: true} ));
 app.use(bodyParser.urlencoded({limit:"30mb", extended: true} ));
 app.use(cors());
 
-app.use('/posts', postRoutes);
-app.use('/user', userRoutes);
 app.options('/*', (_req, res) => {
     res.header('Access-Control-Allow-Origin', '*');
     res.header('Access-Control-Allow-Methods', 'GET, PUT, POST, DELETE, OPTIONS');
     res.header('Access-Control-Allow-Headers', 'Content-Type, Content-Length, Endpoint, Token');
     res.sendStatus(200);
 });
-
+app.use('/posts', postRoutes);
+app.use('/user', userRoutes);
 
 app.get('/', (req, res) => {
     res.send('Hello to Memories API');
