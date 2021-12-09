807d4b21b20ccc6b0903ba0a992b4ab15e14997d
abrave1113 <abrave1113@gmail.com>
2021-12-09 10:06:38 -0500

    mc

--- server/routes/user.js
+++ server/routes/user.js
@@ -8,7 +8,7 @@ router.use('/', (_req, res, next) => {
     res.header('Access-Control-Allow-Origin', '*');
     res.header('Access-Control-Allow-Methods', 'GET, PUT, POST, DELETE, OPTIONS');
     res.header('Access-Control-Allow-Headers', 'Content-Type, Content-Length, Endpoint, Token');
-    res.header('Access-Control-Allow-Credentials', 'true');
+    res.header('Access-Control-Allow-Credentials', true);
     res.sendStatus(200);
     next()
 });
